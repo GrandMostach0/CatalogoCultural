e37df0d1ac3fa22b5b4d6e2d6b7a3dc9f9588577
grandmostach0 <kreedlegend0@gmail.com>
2024-10-12 16:07:51 -0600

    configuracion de la carpeta static funcionando correctamente

--- apps/hola/views.py
+++ apps/hola/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def hola_mundo(request):
-    return render(request, 'hola/base.html');
+    return render(request, 'hola/index.html');
