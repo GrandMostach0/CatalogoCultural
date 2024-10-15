79b06fa0203bb5f8999064ddb77e9ee85e61c694
grandmostach0 <kreedlegend0@gmail.com>
2024-10-15 14:01:17 -0600

    Enlace de los anchors a las paginas correctas

--- apps/hola/urls.py
+++ apps/hola/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hola_mundo, name="hola_mundo"),
+    path('', views.hola_mundo, name="index"),
     path('viewEscuela/', views.viewEscuela, name="viewEscuela"),
     path('viewSesion/', views.viewSesion, name="viewSesion"),
     path('viewPerfil/', views.viewPerfil, name="viewPerfil"),
