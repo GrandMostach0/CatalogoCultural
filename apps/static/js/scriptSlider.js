bae32fa1ff50e14aebf9d9e6ecb74674ad373067
grandmostach0 <kreedlegend0@gmail.com>
2024-11-06 11:48:37 -0600

    responsive de las secciones de actores e instituciones listos

--- apps/static/js/scriptSlider.js
+++ apps/static/js/scriptSlider.js
@@ -1,12 +1,36 @@
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 5, // Cantidad de imágenes permitidas
-    spaceBetween: 20, // Espacio entre imágenes
-    loop: true, // Hace que el carrusel sea infinito
-    pagination: { // Selector de paginación
+    slidesPerView: 1, // Configuración inicial para tamaños pequeños
+    spaceBetween: 10, // Espacio pequeño entre slides
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
         clickable: true,
     },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+        400: {
+            slidesPerView: 1,
+            spaceBetween: 5,
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+        },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+        },
+    }
 });
+
