7c8921c186895323cc2b9dcb5056be22927f94f0
grandmostach0 <kreedlegend0@gmail.com>
2024-12-10 14:11:06 -0600

    enlace del modal para agregar nuevas ubicaciones

---
+++ apps/static/js/panelAdministracion/addUbicacion.js
@@ -0,0 +1,24 @@
+// Esperar a que el DOM se cargue
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("modalUbicación");
+    var abrirModal = document.getElementById("abrirModalUbicacion");
+    var cerrarModal = document.getElementsByClassName("clsModUbicacion")[0];
+    var btnCancelar = document.getElementById("btnCancelar");
+
+    abrirModal.onclick = function(event) {
+        event.preventDefault();
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden";
+    }
+
+    cerrarModal.onclick = function() {
+        modal.style.display = "none";
+        document.body.style.overflow = "auto";
+    }
+
+    btnCancelar.onclick = function() {
+        modal.style.display = "none";
+        document.body.style.overflow = "auto";
+    }
+
+});
