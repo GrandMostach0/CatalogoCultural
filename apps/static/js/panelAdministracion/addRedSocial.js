61273a643aed8f2eb83cf352f559661ad2e83779
grandmostach0 <kreedlegend0@gmail.com>
2025-01-08 21:38:13 -0600

    anexo del modal para editar una red social

--- apps/static/js/panelAdministracion/addRedSocial.js
+++ apps/static/js/panelAdministracion/addRedSocial.js
@@ -19,4 +19,32 @@ document.addEventListener("DOMContentLoaded", function(){
         modal.style.display = "none";
         document.body.style.overflow = "auto";
     }
+
+    /* FUNCION PARA LA PARTE DE EDICION DE LAS REDES SOCIALES*/
+    var abirModalEditRedSocial = document.getElementById("modalEditRedSocial");
+    var clsModalEditRedSocial = document.getElementsByClassName("clsModEditRedSocial")[0];
+    var btnAbrirModalEditRedSocial = document.querySelectorAll(".btnAbrirModalEditRedSocial");
+    var btnCancelarEditRedSocial = document.getElementById("btnCancelarEditRedSocial");
+
+    btnAbrirModalEditRedSocial.forEach(btn => {
+        btn.addEventListener("click", function(event){
+            event.preventDefault();
+            const data_id_atribute = event.target.getAttribute("data-id");
+
+            abirModalEditRedSocial.style.display = "block"
+            document.body.style.overflow = "hidden";
+
+        });
+    });
+
+    clsModalEditRedSocial.addEventListener("click", function(){
+        abirModalEditRedSocial.style.display = "none";
+        document.body.style.overflow = "auto";
+    });
+
+    btnCancelarEditRedSocial.addEventListener("click", function(){
+        abirModalEditRedSocial.style.display = "none";
+        document.body.style.overflow = "auto";
+    });
+
 });
