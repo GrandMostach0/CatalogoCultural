20f1d25f96bb29e6db85331a82a12fdcf265871a
grandmostach0 <kreedlegend0@gmail.com>
2025-01-14 21:43:50 -0600

    anexo de la informacion de los id's extras al consultar los eventos

--- apps/catalogo/serializer.py
+++ apps/catalogo/serializer.py
@@ -2,6 +2,29 @@ from rest_framework import serializers
 from .models import publicacionEventos
 
 class EventoSerializer(serializers.ModelSerializer):
+
+    # Extraer datos de relaciones
+    id_clasificacion_nombre = serializers.ReadOnlyField(source="id_clasificacion.nombre_clasificacion")
+    id_actor_nombre = serializers.ReadOnlyField(source="id_actor.nombre_Actor")
+    id_disciplina_nombre = serializers.ReadOnlyField(source="id_disciplina.nombre_disciplina")
+    id_ubicacionesComunes_nombre = serializers.ReadOnlyField(source="id_ubicacionesComunes.nombre_ubicacion")
+
     class Meta:
         model = publicacionEventos
-        fields = '__all__'
+        fields = [
+            "id",
+            "url_imagen_publicacion",
+            "fecha_creacion_publicacion",
+            "publicacion_aprobada",
+            "titulo_publicacion",
+            "descripcion_publicacion",
+            "fecha_inicio",
+            "hora_inicio",
+            "precio_evento",
+            "puntoVenta",
+            "enlace_venta",
+            "id_clasificacion_nombre",
+            "id_actor_nombre",
+            "id_disciplina_nombre",
+            "id_ubicacionesComunes_nombre",
+        ]
